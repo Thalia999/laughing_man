880c64c1e25c09450794467dca12df9cc622413d
Thalia999 <thaliac000@gmail.com>
2026-04-22 10:39:46 -0400

    starting idea

--- laughing_man.js
+++ laughing_man.js
@@ -1,15 +1,34 @@
 const SQUARE_COUNT=3;
-
+const TIMER_SPEED=500;
+const SPEED =5;
 document.addEventListener("DOMContentLoaded", () =>{
-document.querySelector("#square").adddEventListener("click", () => {
+document.querySelector("#square").addEventListener("click", () => {
   alert("OMG YOU CLICKED ME");
-})
+});
+let box=document.querySelector("#box");
 
 for (let i=0; i<SQUARE_COUNT; i++){
-  let square = document.createElement('div');
+  let square = document.createElement('img');
   square.src = "laughingman.jpg";
   square.alt = "Catch the Laughing Man!";
   square.className="square";
   box.appendChild(square);
 }
+
+Array.from(box.children).forEach((element)=>{
+  let dx= SPEED * Math.random()+2-1;
+  let dy= SPEED * Math.random()+2-1;
+
+
+  let x = parseInt(element.style.left) || 225;
+  let y = parseInt(element.style.top) || 175;
+  setInterval(()=>{
+    x+=dx; 
+    y+=dy;
+
+    element.style.left = x + "px";
+    element.style.top = y + "px";
+  }, TIMER_SPEED)
+});
+
 });
