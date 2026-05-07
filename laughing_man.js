e6b85c1a11615ae0f99f62763add8417c1637c65
Thalia999 <thaliac000@gmail.com>
2026-05-07 05:30:15 -0400

    Have the border change color when hitting the walls Add code so that when we mouseover an img it's src changes to the attached Orochimaru.jpgorochimaru_50x50.jpg / mouseout should set it back to the original image If we mouse over the #square element, change it's background-color instead.

--- laughing_man.js
+++ laughing_man.js
@@ -5,6 +5,14 @@ document.addEventListener("DOMContentLoaded", () =>{
 document.querySelector("#square").addEventListener("click", () => {
   alert("OMG YOU CLICKED ME");
 });
+let squareDiv = document.querySelector("#square");
+    squareDiv.addEventListener("mouseover", () => {
+        squareDiv.style.backgroundColor = newColor();
+    });
+    squareDiv.addEventListener("mouseout", () => {
+        squareDiv.style.backgroundColor = "red";
+    });
+    
 let box=document.querySelector("#box");
 
 for (let i=0; i<SQUARE_COUNT; i++){
@@ -20,18 +28,34 @@ const parent =element.parentElement;
 const maxX = parent.clientWidth - element.clientWidth;
 const maxY = parent.clientHeight - element.clientHeight;
 
-  let dx= SPEED * (Math.random()+2-1);
-  let dy= SPEED * (Math.random()+2-1);
+  let dx = SPEED * (Math.random() * 2 - 1 || 1);
+  let dy = SPEED * (Math.random() * 2 - 1 || 1);
+
+    if (dx === 0) dx = SPEED;
+    if (dy === 0) dy = SPEED;
 
 
   let x = parseInt(element.style.left) || 225;
   let y = parseInt(element.style.top) || 175;
+
+   if (element.tagName === "IMG") {
+            element.addEventListener("mouseover", () => {
+                element.src = "orochimaru.jpg";
+            });
+            element.addEventListener("mouseout", () => {
+                element.src = "laughingman.jpg";
+            });
+        }
+
+
   setInterval(()=>{
 if (x <= 0 || x >= maxX) {
   dx*=-1;
+  element.style.borderColor = newColor();
 }
 if (y <= 0 || y >= maxY) {
   dy*=-1;
+  element.style.borderColor = newColor();
 } 
 
     x+=dx; 
